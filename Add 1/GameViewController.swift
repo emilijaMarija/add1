0226bb82ad3180dd337f43bdb89f2967f3200f8d
emilijaMarija <z.mari.emilija1@gmail.com>
2023-09-11 17:09:42 +0200

    Initial set up

---
+++ Add 1/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Add 1
+//
+//  Created by Emilija Marija on 11.09.23.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
