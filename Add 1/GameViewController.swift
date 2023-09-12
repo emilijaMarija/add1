688bdc59deaa3dc2dfb2f56fbbfad426a7b7b48b
emilijaMarija <z.mari.emilija1@gmail.com>
2023-09-12 14:58:37 +0200

    Add label updating logic

--- Add 1/GameViewController.swift
+++ Add 1/GameViewController.swift
@@ -12,12 +12,20 @@ class GameViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel?
     @IBOutlet weak var numberLabel: UILabel?
     @IBOutlet weak var inputField: UITextField?
+    
+    var score = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func updateScoreLabel() {
+        scoreLabel?.text = String(score)
+    }
 
-
+    func updateNumberLabel() {
+        numberLabel?.text = String.randomNumber(lenght: 4)
+    }
 }
 
